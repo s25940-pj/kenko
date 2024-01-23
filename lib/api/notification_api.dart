7b3372356e7ef8c3a391c2e6df7a284665de7f47
Maksymilian <s25940@pjwstk.edu.pl>
2024-01-23 20:04:43 +0100

    Updated notification system to use date range.

--- lib/api/notification_api.dart
+++ lib/api/notification_api.dart
@@ -4,23 +4,26 @@ import 'package:rxdart/rxdart.dart';
 import 'package:flutter/material.dart';
 import '../screens/reminders/new_reminder_screen.dart';
 
-
-
 class NotificationApi {
   static final _notifications = FlutterLocalNotificationsPlugin();
   static final onNotifications = BehaviorSubject<String?>();
 
-static Future<NotificationDetails> _notificationDetails() async {
-  return NotificationDetails(
-    android: AndroidNotificationDetails(
-      'channel id',
-      'channel name',
-      channelDescription: 'channel description',
-      importance: Importance.max,
-    ),
-    iOS: IOSNotificationDetails(),
-  );
-}
+  static Future<NotificationDetails> _notificationDetails() async {
+    try {
+      return const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'channel id',
+          'channel name',
+          channelDescription: 'channel description',
+          importance: Importance.max,
+        ),
+        iOS: IOSNotificationDetails(),
+      );
+    } catch (e) {
+      print('Error creating notification details: $e');
+      throw e;
+    }
+  }
 
   static Future init({bool initSheduled = false}) async {
     final android = AndroidInitializationSettings('@mipmap/ic_launcher');
@@ -32,47 +35,62 @@ static Future<NotificationDetails> _notificationDetails() async {
     );
   }
 
-  static Future<void> scheduleNotificationAtTime({
-    required TimeOfDay timeOfDay,
+  static Future<void> scheduleMultipleNotificationsAtDateTimeRange({
+    required DateTimeRange dateRange,
+    required TimeOfDay selectedTime,
     required String medicationName,
     required int dosage,
   }) async {
-    final now = DateTime.now();
-    final selectedDateTime = DateTime(
-      now.year,
-      now.month,
-      now.day,
-      timeOfDay.hour,
-      timeOfDay.minute,
-    );
+    final DateTime startDate = dateRange.start;
+    final DateTime endDate = dateRange.end;
+    final int numberOfDays = endDate.difference(startDate).inDays;
 
-    final int notificationId = now.microsecondsSinceEpoch % 1000000;
-    final String notificationTitle = 'Take your $medicationName';
-    final String notificationBody = 'Take $dosage of $medicationName';
-    final String notificationPayload = 'Reminder Payload';
-
-    print('Notification ID: $notificationId');
-    print('Notification Title: $notificationTitle');
-    print('Notification Body: $notificationBody');
-    print('Selected DateTime: $selectedDateTime');
-    try{ 
-    await _notifications.zonedSchedule(
-      notificationId,
-      notificationTitle,
-      notificationBody,
-      tz.TZDateTime.from(selectedDateTime, tz.local),
-      await _notificationDetails(),
-      androidAllowWhileIdle: true,
-      payload: notificationPayload,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-    );
-    print('Notification scheduled successfully!');
-    } catch (e, stackTrace) {
-  print('Error scheduling notification: $e');
-  print('Stack trace: $stackTrace');
-}
+    for (int i = 0; i <= numberOfDays; i++) {
+      final DateTime currentDate = startDate.add(Duration(days: i));
+      scheduleNotificationAtDateTime(
+        selectedDate: currentDate,
+        selectedTime: selectedTime,
+        medicationName: medicationName,
+        dosage: dosage,
+      );
+    }
   }
 
+  static Future<void> scheduleNotificationAtDateTime({
+    required DateTime selectedDate,
+    required TimeOfDay selectedTime,
+    required String medicationName,
+    required int dosage,
+  }) async {
+    final int notificationId = DateTime.now().microsecondsSinceEpoch % 1000000;
+    final String notificationTitle = 'Take your $medicationName';
+    final String notificationBody = 'Take $dosage of $medicationName';
+    const String notificationPayload = 'Reminder Payload';
 
+    final selectedDateTime = DateTime(
+      selectedDate.year,
+      selectedDate.month,
+      selectedDate.day,
+      selectedTime.hour,
+      selectedTime.minute,
+    );
+
+    try {
+      await _notifications.zonedSchedule(
+        notificationId,
+        notificationTitle,
+        notificationBody,
+        tz.TZDateTime.from(selectedDateTime, tz.local),
+        await _notificationDetails(),
+        androidAllowWhileIdle: true,
+        payload: notificationPayload,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+      );
+      print('Notification scheduled successfully!');
+    } catch (e, stackTrace) {
+      print('Error scheduling notification: $e');
+      print('Stack trace: $stackTrace');
+    }
+  }
 }
